7452e0d5a3543710bd9cc6f7d4db5431e21aeb7e
RIZWAN AKHTAR <riziecd@gmail.com>
2021-05-23 23:00:24 +0500

    code maked for localhost

--- password.php
+++ password.php
@@ -3,17 +3,17 @@
     <meta content="IE=edge" http-equiv="X-UA-Compatible">
     <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
 
-    <link href="https://assets-fix-me.cabify.com/837e2d3cb95d45e2e295bf556fea5bc9.ico?vsn=d" rel="shortcut icon" sizes="16x16 32x32 64x64">
-    <link href="https://assets-fix-me.cabify.com/c3ce6d5637255f4c6fff314b9cec554a.png?vsn=d" rel="shortcut icon" sizes="16x16" type="image/png">
-    <link href="https://assets-fix-me.cabify.com/31a483a3f091374043c2d9a2899f1e11.png?vsn=d" rel="shortcut icon" sizes="32x32" type="image/png">
-    <link href="https://assets-fix-me.cabify.com/0c3e990e6a062f1fd6d7b23d7b987cb9.png?vsn=d" rel="apple-touch-icon">
+    <link href="837e2d3cb95d45e2e295bf556fea5bc9.ico?vsn=d" rel="shortcut icon" sizes="16x16 32x32 64x64">
+    <link href="c3ce6d5637255f4c6fff314b9cec554a.png?vsn=d" rel="shortcut icon" sizes="16x16" type="image/png">
+    <link href="31a483a3f091374043c2d9a2899f1e11.png?vsn=d" rel="shortcut icon" sizes="32x32" type="image/png">
+    <link href="0c3e990e6a062f1fd6d7b23d7b987cb9.png?vsn=d" rel="apple-touch-icon">
 
-    <link href="https://assets-fix-me.cabify.com/app.8acc4b661bdcdba5de71.css?vsn=d" rel="stylesheet">
+    <link href="app.8acc4b661bdcdba5de71.css?vsn=d" rel="stylesheet">
 
     <title>Cabify</title>
-  <script charset="utf-8" src="https://assets-fix-me.cabify.com/14.d762be9d3af6fb7603bd.js"></script><script charset="utf-8" src="https://assets-fix-me.cabify.com/15.d0af1ea8a19adea06b62.js"></script><script charset="utf-8" src="https://assets-fix-me.cabify.com/5.21406910294f8afe3989.js"></script>
+  <script charset="utf-8" src="14.d762be9d3af6fb7603bd.js"></script><script charset="utf-8" src="15.d0af1ea8a19adea06b62.js"></script><script charset="utf-8" src="5.21406910294f8afe3989.js"></script>
   <style>
-	/*! CSS Used from: https://assets-fix-me.cabify.com/app.8acc4b661bdcdba5de71.css?vsn=d */
+	/*! CSS Used from: app.8acc4b661bdcdba5de71.css?vsn=d */
 *{box-sizing:border-box;outline:none;}
 a,article,body,div,form,label,p,span{margin:0;padding:0;border:0;vertical-align:baseline;font:100%;}
 article{display:block;}
@@ -25,7 +25,7 @@ input:-ms-input-placeholder,input::-ms-input-placeholder{opacity:1;}
 input::placeholder{opacity:1;}
 .login-header-action:before,.login-splash-logo:before{margin-top:-2px;vertical-align:middle;text-transform:none;font-family:iconfont;font-weight:400;font-style:normal;font-variant:normal;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;line-height:1;speak:none;}
 .loaded .spinner{display:none;}
-.spinner{margin-top:10%;background:url(https://assets-fix-me.cabify.com/c3cce433a6e30ef49079c4877d156f05.png) 50% no-repeat;background-size:cover;}
+.spinner{margin-top:10%;background:url(c3cce433a6e30ef49079c4877d156f05.png) 50% no-repeat;background-size:cover;}
 .spinner{position:absolute;margin-left:-8px;left:50%;height:16px;width:16px;-webkit-animation:rotate .8s infinite linear;animation:rotate .8s infinite linear;}
 ::-moz-selection{color:#2e2545;background:#c1b2ff;}
 ::selection{color:#2e2545;background:#c1b2ff;}
@@ -88,10 +88,10 @@ input{font-family:CabifyCircular,Avenir Next,Avenir,Segoe UI,sans-serif;}
 @-webkit-keyframes rotate{0%{-webkit-transform:rotate(0deg);transform:rotate(0deg);}to{-webkit-transform:rotate(1turn);transform:rotate(1turn);}}
 @keyframes rotate{0%{-webkit-transform:rotate(0deg);transform:rotate(0deg);}to{-webkit-transform:rotate(1turn);transform:rotate(1turn);}}
 /*! CSS Used fontfaces */
-@font-face{font-family:iconfont;src:url(https://assets-fix-me.cabify.com/062782c83cfff7b090b504e8a029ae08.ttf) format("truetype"),url(https://assets-fix-me.cabify.com/3ef3396b854de4295701d4144ab00db1.woff) format("woff"),url(https://assets-fix-me.cabify.com/fb204edf86ad2282bee76d8c6a2b59de.svg#iconfont) format("svg");font-weight:400;font-style:normal;}
-@font-face{font-family:CabifyCircular;src:url(https://assets-fix-me.cabify.com/4682a80eb8678550a9041112d1bba2b6.eot);src:url(https://assets-fix-me.cabify.com/4682a80eb8678550a9041112d1bba2b6.eot#iefix) format("embedded-opentype"),url(https://assets-fix-me.cabify.com/d4e9a293c572c37986a7f911ad78a912.woff) format("woff"),url(https://assets-fix-me.cabify.com/0103214f69191c450fcf4fb323ead350.woff2) format("woff2");font-weight:600;font-style:normal;}
-@font-face{font-family:CabifyCircular;src:url(https://assets-fix-me.cabify.com/cdf1af7d46e884a4a7d70e738380641f.eot);src:url(https://assets-fix-me.cabify.com/cdf1af7d46e884a4a7d70e738380641f.eot#iefix) format("embedded-opentype"),url(https://assets-fix-me.cabify.com/6e023f09687fc4f8d87705c08e421a2a.woff) format("woff"),url(https://assets-fix-me.cabify.com/9b6f88d183407d5b2c9d2e03a390e93d.woff2) format("woff2");font-weight:400;font-style:normal;}
-@font-face{font-family:CabifyCircular;src:url(https://assets-fix-me.cabify.com/20e023112f736f033cb6f42eebbcfa8f.eot);src:url(https://assets-fix-me.cabify.com/20e023112f736f033cb6f42eebbcfa8f.eot#iefix) format("embedded-opentype"),url(https://assets-fix-me.cabify.com/6f97a71f89521c0b690dd4285cdf9fd4.woff) format("woff"),url(https://assets-fix-me.cabify.com/027ea6bae887b76521840869ddc1ebac.woff2) format("woff2");font-weight:300;font-style:normal;}
+@font-face{font-family:iconfont;src:url(062782c83cfff7b090b504e8a029ae08.ttf) format("truetype"),url(3ef3396b854de4295701d4144ab00db1.woff) format("woff"),url(fb204edf86ad2282bee76d8c6a2b59de.svg#iconfont) format("svg");font-weight:400;font-style:normal;}
+@font-face{font-family:CabifyCircular;src:url(4682a80eb8678550a9041112d1bba2b6.eot);src:url(4682a80eb8678550a9041112d1bba2b6.eot#iefix) format("embedded-opentype"),url(d4e9a293c572c37986a7f911ad78a912.woff) format("woff"),url(0103214f69191c450fcf4fb323ead350.woff2) format("woff2");font-weight:600;font-style:normal;}
+@font-face{font-family:CabifyCircular;src:url(cdf1af7d46e884a4a7d70e738380641f.eot);src:url(cdf1af7d46e884a4a7d70e738380641f.eot#iefix) format("embedded-opentype"),url(6e023f09687fc4f8d87705c08e421a2a.woff) format("woff"),url(9b6f88d183407d5b2c9d2e03a390e93d.woff2) format("woff2");font-weight:400;font-style:normal;}
+@font-face{font-family:CabifyCircular;src:url(20e023112f736f033cb6f42eebbcfa8f.eot);src:url(20e023112f736f033cb6f42eebbcfa8f.eot#iefix) format("embedded-opentype"),url(6f97a71f89521c0b690dd4285cdf9fd4.woff) format("woff"),url(027ea6bae887b76521840869ddc1ebac.woff2) format("woff2");font-weight:300;font-style:normal;}
   </style>
   </head>
   
